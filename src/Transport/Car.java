26066f8e0a50a00df2a4f1745ba4efc5599aa59c
b.Mashrapov <b_mashrapov@mail.ru>
2022-12-25 19:13:44 +0100

    Initial commit

---
+++ src/Transport/Car.java
@@ -0,0 +1,7 @@
+package Transport;
+
+public class Car extends Transport {
+    public Car(String brand, String model, double engineVolume) {
+        super(brand, model, engineVolume);
+    }
+}
---
+++ src/Transport/Truck.java
@@ -0,0 +1,7 @@
+package Transport;
+
+public class Truck extends Transport{
+    public Truck(String brand, String model, double engineVolume) {
+        super(brand, model, engineVolume);
+    }
+}
